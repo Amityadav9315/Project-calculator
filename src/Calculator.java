ee372daf7c1ed21e4ff52c9578e7fc5468aa6765
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-05-15 17:48:46 +0530

    calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Calculator {
-}
+import java.lang.*;
+import java.util.*;
+public class Calculator {
+        public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the valuse of a");
+            int a=sc.nextInt();
+            System.out.println("Enter the valuse of b");
+            int b=sc.nextInt();
+            System.out.println("Enter the operator");
+            char op=sc.next().charAt(0);
+            switch(op){
+                case '+':
+                    System.out.println(a+b);
+                    break;
+                case '-':
+                    System.out.println(a-b);
+                    break;
+                case '*':
+                    System.out.println(a*b);
+                    break;
+                case '/':
+                    System.out.println(a%b);
+                    break;
+                default:
+                    System.out.println("invalid character");
+            }
+
+            }
+        }
+
+
